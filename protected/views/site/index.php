b2c33c168956e95e0d7cf7a3bb767f9d91fe09cb
tongfreedom <tongfreedom@gmail.com>
2015-06-09 10:48:35 +0700

    form

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -4,17 +4,118 @@
 $this->pageTitle=Yii::app()->name;
 ?>
 
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+<h1>ฟอร์มสมัครเรียน</h1>
 
-<p>Congratulations! You have successfully created your Yii application.</p>
 
-<p>You may change the content of this page by modifying the following two files:</p>
-<ul>
-	<li>View file: <code><?php echo __FILE__; ?></code></li>
-	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
-</ul>
+<?php
+/* @var $this StudentsController */
+/* @var $model Students */
+/* @var $form CActiveForm */
+?>
 
-<p>For more details on how to further develop this application, please read
-the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'students-form',
+	// Please note: When you enable ajax validation, make sure the corresponding
+	// controller action is handling ajax validation correctly.
+	// There is a call to performAjaxValidation() commented in generated controller code.
+	// See class documentation of CActiveForm for details on this.
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'levels_id'); ?>
+		<?php echo $form->textField($model,'levels_id'); ?>
+		<?php echo $form->error($model,'levels_id'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'fisrt_name'); ?>
+		<?php echo $form->textField($model,'fisrt_name',array('size'=>60,'maxlength'=>100)); ?>
+		<?php echo $form->error($model,'fisrt_name'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'las_name'); ?>
+		<?php echo $form->textField($model,'las_name',array('size'=>60,'maxlength'=>100)); ?>
+		<?php echo $form->error($model,'las_name'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'email'); ?>
+		<?php echo $form->textField($model,'email',array('size'=>60,'maxlength'=>100)); ?>
+		<?php echo $form->error($model,'email'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'tel'); ?>
+		<?php echo $form->textField($model,'tel',array('size'=>45,'maxlength'=>45)); ?>
+		<?php echo $form->error($model,'tel'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'prefix_id'); ?>
+		<?php echo $form->textField($model,'prefix_id'); ?>
+		<?php echo $form->error($model,'prefix_id'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'parent_first_name'); ?>
+		<?php echo $form->textField($model,'parent_first_name',array('size'=>60,'maxlength'=>100)); ?>
+		<?php echo $form->error($model,'parent_first_name'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'parent_last_name'); ?>
+		<?php echo $form->textField($model,'parent_last_name',array('size'=>60,'maxlength'=>100)); ?>
+		<?php echo $form->error($model,'parent_last_name'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'parent_tel'); ?>
+		<?php echo $form->textField($model,'parent_tel',array('size'=>45,'maxlength'=>45)); ?>
+		<?php echo $form->error($model,'parent_tel'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'parent_email'); ?>
+		<?php echo $form->textField($model,'parent_email',array('size'=>60,'maxlength'=>100)); ?>
+		<?php echo $form->error($model,'parent_email'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'created'); ?>
+		<?php echo $form->textField($model,'created'); ?>
+		<?php echo $form->error($model,'created'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'address'); ?>
+		<?php echo $form->textField($model,'address',array('size'=>60,'maxlength'=>255)); ?>
+		<?php echo $form->error($model,'address'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'tambons_id'); ?>
+		<?php echo $form->textField($model,'tambons_id'); ?>
+		<?php echo $form->error($model,'tambons_id'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'status'); ?>
+		<?php echo $form->textField($model,'status',array('size'=>45,'maxlength'=>45)); ?>
+		<?php echo $form->error($model,'status'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
